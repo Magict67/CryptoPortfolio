31979e101fb73f7e130ea50ba5553af95f1f755e
magict67 <christopheradonaithomas@gmail.com>
2026-01-02 20:14:52 -0800

    update models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Coin(models.Model):
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10)
+    quantity = models.DecimalField(max_digits=20, decimal_places=10)
+    price_purchased = models.DecimalField(max_digits=20, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} ({self.symbol})"
