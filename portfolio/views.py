7629b57dc1ddf8a13723ba8e58a176c3e72198ad
magict67 <christopheradonaithomas@gmail.com>
2026-01-05 18:58:07 -0800

    Add views.py math and remove notes

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,22 +3,26 @@ from .models import Coin
 import requests
 
 def portfolio_list(request):
-    #  get  saved coins from  database
     coins = Coin.objects.all()
-    
-    # Working test code to get price
     url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
     
     try:
         response = requests.get(url)
         data = response.json()
-        # Pull price same as test
         live_price = data['bitcoin']['usd']
     except:
-        # Show this if down
-        live_price = "Network Error"
+        live_price = 0
 
-    # Deliver coin and price
+    # NEW: Calculate values for each coin
+    for coin in coins:
+        # If it's Bitcoin, calculate the value
+        if coin.symbol.upper() == 'BTC':
+            coin.current_value = float(coin.quantity) * live_price
+            coin.profit = coin.current_value - (float(coin.quantity) * float(coin.price_purchased))
+        else:
+            coin.current_value = 0
+            coin.profit = 0
+
     return render(request, 'portfolio/portfolio_list.html', {
         'coins': coins, 
         'live_price': live_price
