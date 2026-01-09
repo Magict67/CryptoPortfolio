9547da391f0675092943015d7dfc71e1979fc0ac
magict67 <christopheradonaithomas@gmail.com>
2026-01-09 08:28:04 -0800

    add history follow, collect

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,35 +5,42 @@ import requests
 def portfolio_list(request):
     coins = Coin.objects.all()
     
-    # We ask for BOTH bitcoin and ethereum prices in one go
+    # API URLs for current prices and 7-day history
     url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd"
+    chart_url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7&interval=daily"
     
     try:
+        # 1. Fetch Current Prices
         response = requests.get(url)
         data = response.json()
         prices = {
             'BTC': data.get('bitcoin', {}).get('usd', 0),
             'ETH': data.get('ethereum', {}).get('usd', 0)
         }
+        
+        # 2. Fetch Historical Data for the Chart
+        chart_response = requests.get(chart_url)
+        chart_data = chart_response.json()
+        # Extracting the price values into a simple list of numbers
+        prices_list = [item[1] for item in chart_data.get('prices', [])]
     except:
         prices = {'BTC': 0, 'ETH': 0}
+        prices_list = []
 
     total_portfolio_value = 0 
-
     for coin in coins:
-        # Look up the price based on the symbol (BTC or ETH)
         live_price = prices.get(coin.symbol.upper(), 0)
-        
         qty = float(coin.quantity)
         bought_at = float(coin.price_purchased)
-        
         coin.current_value = qty * live_price
         coin.profit = coin.current_value - (qty * bought_at)
         total_portfolio_value += coin.current_value 
 
+    # We are now sending 'prices_list' to the HTML
     return render(request, 'portfolio/portfolio_list.html', {
         'coins': coins, 
-        'total_value': total_portfolio_value
+        'total_value': total_portfolio_value,
+        'prices_list': prices_list 
     })
 
 def add_coin(request):
